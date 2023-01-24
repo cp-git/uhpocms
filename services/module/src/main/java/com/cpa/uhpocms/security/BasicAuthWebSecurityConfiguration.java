6ef68f562c68e4f938ab5a3c44498134e53d0114
Shradha2105 <shradha.p@cloudpointamerica.com>
2023-01-24 12:16:22 +0530

    basic authentication modification

--- services/module/src/main/java/com/cpa/uhpocms/security/BasicAuthWebSecurityConfiguration.java
+++ services/module/src/main/java/com/cpa/uhpocms/security/BasicAuthWebSecurityConfiguration.java
@@ -6,38 +6,77 @@ package com.cpa.uhpocms.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer.UserDetailsBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+
+
+
+@Configuration
+@EnableWebSecurity
 public class BasicAuthWebSecurityConfiguration {
 
-	@Autowired
-	 private AppBasicAuthenticationEntryPoint authenticationEntryPoint;
+//	@Autowired
+//	 private AppBasicAuthenticationEntryPoint authenticationEntryPoint;
+//	
+//	
+//	
+//	
+//	@Bean
+//	  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//	    http.authorizeRequests()
+//	        .antMatchers("/public").permitAll()
+//	        .anyRequest().authenticated()
+//	        .and()
+//	        .httpBasic()
+//	        .authenticationEntryPoint(authenticationEntryPoint);
+//	    return http.build();
+//	  }
+//	
+//
+//	 
+
+	 
 	
-	
-	@Bean
-	  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	    http.authorizeRequests()
-	        .antMatchers("/public").permitAll()
+	 @Bean
+	    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+	         http
+	        .cors().and()
+	        .csrf().disable().authorizeHttpRequests()
+	        .antMatchers().permitAll()
 	        .anyRequest().authenticated()
 	        .and()
-	        .httpBasic()
-	        .authenticationEntryPoint(authenticationEntryPoint);
-	    return http.build();
-	  }
-	
-
+	        .formLogin();
+	         return http.build();
+	    }
+	 
+	 
+	 @Autowired
+	    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		 String password = "123456"; 
+		 PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(); 
+		 String encodedPassword = passwordEncoder.encode(password);
+	        auth
+	          .inMemoryAuthentication()
+	          .withUser("user1")
+	          .password(encodedPassword)
+	          .authorities("ROLE_USER");
+	    }
+	 
 	 
 	  @Bean
 	  public PasswordEncoder passwordEncoder() {
-	    return new BCryptPasswordEncoder(8);
+	    return new BCryptPasswordEncoder();
 	  }
-	  
 	  
 }
