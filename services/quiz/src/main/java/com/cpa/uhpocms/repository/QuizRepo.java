7638d377084e80ca72cb4c8aafacccce38d75d50
AnmeshR <asrapol18@gmail.com>
2023-10-18 12:21:41 +0530

    changes in quiz

--- services/quiz/src/main/java/com/cpa/uhpocms/repository/QuizRepo.java
+++ services/quiz/src/main/java/com/cpa/uhpocms/repository/QuizRepo.java
@@ -61,6 +61,6 @@ public interface QuizRepo extends JpaRepository<Quiz, Integer> {
 	public List<Quiz> getAllInactiveQuizzesByTeacherId(int profileId);
 	
 	//GET ALL QUIZZESS BY TEACHER ID
-	@Query(value = "SELECT quiz.* FROM public.teacher_quiz quiz JOIN public.teacher_course_assigntoteacher assigncourses ON quiz.courseid_id = assigncourses.course_id JOIN public.teacher_course tc ON quiz.courseid_id = tc.courseid AND tc.isactive = true JOIN public.teacher_module tm ON quiz.module_id = tm.moduleid AND tm.isactive = true WHERE assigncourses.profile_id = ?1 AND quiz.isactive = true", nativeQuery = true)
+	@Query(value = "SELECT * FROM teacher_quiz,teacher_module,teacher_course,admin_department,teacher_course_assigntoteacher WHERE teacher_quiz.module_id= teacher_module.moduleid AND teacher_module.courseid_id = teacher_course.courseid AND teacher_course.instid = admin_department.institutionid  AND teacher_quiz.courseid_id = teacher_course_assigntoteacher.course_id AND admin_department.isactive=true AND teacher_course.isactive=true  AND teacher_module.isactive=true AND teacher_course_assigntoteacher.profile_id=?1", nativeQuery = true)
 	public List<Quiz> getAllQuizzesByTeacherId(int profileId);
 }
